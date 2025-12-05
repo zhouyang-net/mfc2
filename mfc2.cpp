55a6602ed19fff2eb5f4c792df623be2cf6abac2
zy-rog18\zhouyang <85017304@qq.com>
2025-12-05 23:54:23 +0800

    添加项目文件。

---
+++ mfc2.cpp
@@ -0,0 +1,186 @@
+﻿
+// mfc2.cpp: 定义应用程序的类行为。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "afxwinappex.h"
+#include "afxdialogex.h"
+#include "mfc2.h"
+#include "MainFrm.h"
+
+#include "ChildFrm.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Cmfc2App
+
+BEGIN_MESSAGE_MAP(Cmfc2App, CWinApp)
+	ON_COMMAND(ID_APP_ABOUT, &Cmfc2App::OnAppAbout)
+	ON_COMMAND(ID_FILE_NEW, &Cmfc2App::OnFileNew)
+END_MESSAGE_MAP()
+
+
+// Cmfc2App 构造
+
+Cmfc2App::Cmfc2App() noexcept
+{
+
+	// 支持重新启动管理器
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_ALL_ASPECTS;
+#ifdef _MANAGED
+	// 如果应用程序是利用公共语言运行时支持(/clr)构建的，则: 
+	//     1) 必须有此附加设置，“重新启动管理器”支持才能正常工作。
+	//     2) 在您的项目中，您必须按照生成顺序向 System.Windows.Forms 添加引用。
+	System::Windows::Forms::Application::SetUnhandledExceptionMode(System::Windows::Forms::UnhandledExceptionMode::ThrowException);
+#endif
+
+	// TODO: 将以下应用程序 ID 字符串替换为唯一的 ID 字符串；建议的字符串格式
+	//为 CompanyName.ProductName.SubProduct.VersionInformation
+	SetAppID(_T("mfc2.AppID.NoVersion"));
+
+	// TODO:  在此处添加构造代码，
+	// 将所有重要的初始化放置在 InitInstance 中
+}
+
+// 唯一的 Cmfc2App 对象
+
+Cmfc2App theApp;
+
+
+// Cmfc2App 初始化
+
+BOOL Cmfc2App::InitInstance()
+{
+	// 如果一个运行在 Windows XP 上的应用程序清单指定要
+	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
+	//则需要 InitCommonControlsEx()。  否则，将无法创建窗口。
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// 将它设置为包括所有要在应用程序中使用的
+	// 公共控件类。
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinApp::InitInstance();
+
+
+	// 初始化 OLE 库
+	if (!AfxOleInit())
+	{
+		AfxMessageBox(IDP_OLE_INIT_FAILED);
+		return FALSE;
+	}
+
+	AfxEnableControlContainer();
+
+	EnableTaskbarInteraction(FALSE);
+
+	// 使用 RichEdit 控件需要 AfxInitRichEdit2()
+	// AfxInitRichEdit2();
+
+	// 标准初始化
+	// 如果未使用这些功能并希望减小
+	// 最终可执行文件的大小，则应移除下列
+	// 不需要的特定初始化例程
+	// 更改用于存储设置的注册表项
+	// TODO: 应适当修改该字符串，
+	// 例如修改为公司或组织名
+	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+
+
+	// 若要创建主窗口，此代码将创建新的框架窗口
+	// 对象，然后将其设置为应用程序的主窗口对象
+	CMDIFrameWnd* pFrame = new CMainFrame;
+	if (!pFrame)
+		return FALSE;
+	m_pMainWnd = pFrame;
+	// 创建主 MDI 框架窗口
+	if (!pFrame->LoadFrame(IDR_MAINFRAME))
+		return FALSE;
+	// 试图加载共享 MDI 菜单和快捷键表
+	//TODO: 添加附加成员变量，并加载对
+	//	应用程序可能需要的附加菜单类型的调用
+	HINSTANCE hInst = AfxGetResourceHandle();
+	m_hMDIMenu  = ::LoadMenu(hInst, MAKEINTRESOURCE(IDR_mfc2TYPE));
+	m_hMDIAccel = ::LoadAccelerators(hInst, MAKEINTRESOURCE(IDR_mfc2TYPE));
+
+
+
+
+	// 主窗口已初始化，因此显示它并对其进行更新
+	pFrame->ShowWindow(m_nCmdShow);
+	pFrame->UpdateWindow();
+
+	return TRUE;
+}
+
+int Cmfc2App::ExitInstance()
+{
+	//TODO: 处理可能已添加的附加资源
+	if (m_hMDIMenu != nullptr)
+		FreeResource(m_hMDIMenu);
+	if (m_hMDIAccel != nullptr)
+		FreeResource(m_hMDIAccel);
+
+	AfxOleTerm(FALSE);
+
+	return CWinApp::ExitInstance();
+}
+
+// Cmfc2App 消息处理程序
+
+void Cmfc2App::OnFileNew()
+{
+	CMainFrame* pFrame = STATIC_DOWNCAST(CMainFrame, m_pMainWnd);
+	// 创建新的 MDI 子窗口
+	pFrame->CreateNewChild(
+		RUNTIME_CLASS(CChildFrame), IDR_mfc2TYPE, m_hMDIMenu, m_hMDIAccel);
+}
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg() noexcept;
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() noexcept : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+// 用于运行对话框的应用程序命令
+void Cmfc2App::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+// Cmfc2App 消息处理程序
+
+
+
---
+++ mfc2.h
@@ -0,0 +1,39 @@
+﻿
+// mfc2.h: mfc2 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// Cmfc2App:
+// 有关此类的实现，请参阅 mfc2.cpp
+//
+
+class Cmfc2App : public CWinApp
+{
+public:
+	Cmfc2App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+protected:
+	HMENU  m_hMDIMenu;
+	HACCEL m_hMDIAccel;
+
+public:
+	afx_msg void OnAppAbout();
+	afx_msg void OnFileNew();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cmfc2App theApp;
